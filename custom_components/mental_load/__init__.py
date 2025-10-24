82e20d87955d166c5768b02f0d946710b7341a41
Sduniii <tobias.sdun@gmail.com>
2025-10-24 14:11:32 +0200

    start

--- custom_components/mental_load/__init__.py
+++ custom_components/mental_load/__init__.py
@@ -1,42 +1,8 @@
 """The Mental load Assistant integration."""
-from __future__ import annotations
+DOMAIN = "mental_load"
 
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.config_entry_oauth2_flow import (
-    async_get_config_entry_implementation,
-)
+def setup(hass, config):
+    hass.states.async_set("mental_load.world", "Sduniii")
 
-from .const import DOMAIN
-from .coordinator import MentalLoadCoordinator
-
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up Mental load Assistant from a config entry."""
-    
-    # Holt die OAuth-Implementierung
-    implementation = await async_get_config_entry_implementation(hass, entry)
-    
-    # Erstellt die Coordinator-Instanz
-    coordinator = MentalLoadCoordinator(hass, entry, implementation)
-
-    await coordinator.async_config_entry_first_refresh()
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
-
-    entry.async_on_unload(entry.add_update_listener(options_update_listener))
-    await hass.config_entries.async_forward_entry_setups(entry, ["todo"])
-
+    # Return boolean to indicate that initialization was successful.
     return True
-
-
-async def options_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Handle options update."""
-    await hass.config_entries.async_reload(entry.entry_id)
-
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
-    if unloaded := await hass.config_entries.async_unload_platforms(entry, ["todo"]):
-        hass.data[DOMAIN].pop(entry.entry_id)
-        return unloaded
-    return False
--- custom_components/mental_load/const.py
+++
@@ -1,8 +0,0 @@
-"""Constants for the Mental load Assistant integration."""
-
-# The domain of your integration. Should be unique amongst all integrations.
-DOMAIN = "mental_load"
-
-# Scopes provide access to certain parts of the Google API.
-# We are requesting read-only access to the user's calendar.
-OAUTH2_SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
